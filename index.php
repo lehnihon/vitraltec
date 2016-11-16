004faebc0b8c98e45831f750d56a54e88e5bd7ec
Leandro <lehnihon@hotmail.com>
2016-11-16 16:07:35 -0200

    endereço correto

--- index.php
+++ index.php
@@ -71,10 +71,10 @@ get_header(); ?>
 				<div class="col-md-12">
 					<h2 class="branco">Nossa localização</h2>
 					<p>
-						Rua Capitão Eugênio de Macedo, 353 - Itaim Paulista - São Paulo - SP
+						Rua Vicente Prado, 117 - Bela Vista - São Paulo - SP
 					</p>
 					<p>
-						<strong>Telefone.: </strong>11 2561-5068 | 11 98359-1900  <strong>Email.: </strong>vendas@vitraltec.com.br
+						<strong>Telefone.: </strong>11 2887-0327  |  11 95340-7221  <strong>Email.: </strong>vendas@vitraltec.com.br
 					</p>
 				</div>
 			</div>
@@ -82,7 +82,7 @@ get_header(); ?>
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-12 videoWrapper">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.886412271463!2d-46.40424858551352!3d-23.500600465235326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce638221e3bf09%3A0xc096822a20507e28!2sRua+Capit%C3%A3o+Eugenio+de+Macedo%2C+353+-+Vila+Silva+Teles%2C+S%C3%A3o+Paulo+-+SP%2C+08110-150!5e0!3m2!1spt-BR!2sbr!4v1478214412715" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>				</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.3351563227593!2d-46.643565184559634!3d-23.556403367295395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59ae1f031eb1%3A0x98df301bf2fac1d3!2sR.+Vicente+Prado%2C+117+-+Bela+Vista%2C+S%C3%A3o+Paulo+-+SP%2C+01321-020!5e0!3m2!1spt-BR!2sbr!4v1479318132036" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>			</div>
 			</div>			
 		</div>
 	</section>
--- page-contato.php
+++ page-contato.php
@@ -13,15 +13,15 @@ get_header(); ?>
 				<div class="col-md-6 text-left ">
 					<br>
 					<div class="videoWrapperb">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.886412271463!2d-46.40424858551352!3d-23.500600465235326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce638221e3bf09%3A0xc096822a20507e28!2sRua+Capit%C3%A3o+Eugenio+de+Macedo%2C+353+-+Vila+Silva+Teles%2C+S%C3%A3o+Paulo+-+SP%2C+08110-150!5e0!3m2!1spt-BR!2sbr!4v1478214412715" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>					</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.3351563227593!2d-46.643565184559634!3d-23.556403367295395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59ae1f031eb1%3A0x98df301bf2fac1d3!2sR.+Vicente+Prado%2C+117+-+Bela+Vista%2C+S%C3%A3o+Paulo+-+SP%2C+01321-020!5e0!3m2!1spt-BR!2sbr!4v1479318132036" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>			</div>
 					<div class="quadro">
 					<p>
-						Rua Capitão Eugênio de Macedo, 353 - Itaim Paulista - São Paulo - SP
+						Rua Vicente Prado, 117 - Bela Vista - São Paulo - SP
 					</p>
 					<p>
-						11 2561-5068 | 11 98359-1900
+						11 2887-0327  |  11 95340-7221
 					</p>
-					<p>carolina@vitraltec.com.br</p>							
+					<p>vendas@vitraltec.com.br</p>							
 					</div>
 				</div>
 			</div>	
